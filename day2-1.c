d607ab3c61a61dfb57107d4f44dd82931c79f528
Rutwick <147582744+Rutwickthakare@users.noreply.github.com>
2023-10-14 00:49:39 +0530

    Add files via upload

---
+++ day2-1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int checkpoint (void)
+
+int main(void)
+
+{
+    int a =checkinput();
+    int b =checkinput();
+    int c =checkinput();
+    int d =checkinput();
+
+    printf("\n 44,55,42,89;\n");
+    printf("A = %d\n",a);
+    printf("B = %d\n",b);
+    printf("C = %d\n",c);
+    printf("D = %d\n",d);
+    return 0;
+}
